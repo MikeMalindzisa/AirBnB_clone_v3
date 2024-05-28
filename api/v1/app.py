04a124637f333aab20e11089f4ffb86efa173dd0
MikeMalindzisa <machaweml@gmail.com>
2024-05-27 17:49:32 -0700

    v1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    This starts a flask application
+    This starts a lask application
 """
 
 from flask import Flask
@@ -13,16 +13,26 @@ app = Flask(__name__)
 # Register the blueprint `app_views` to your Flask instance `app`
 app.register_blueprint(app_views)
 
-# Declare a method to handle `@app.teardown_appcontext` that calls `storage.close()`
+
+# Declare a method to handle `@app.teardown_appcontext`
+# that calls `storage.close()`
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def error(e):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
 
 # Inside if __name__ == "__main__":, run your Flask server (`app`)
 if __name__ == "__main__":
     import os
 
-    # Get host and port from environment variables, fallback to default values if not defined
+    # Get host and port from environment variables, fallback to
+    # default values if not defined
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
 
